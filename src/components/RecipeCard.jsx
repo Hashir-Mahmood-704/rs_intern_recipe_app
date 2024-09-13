8b8aa824aab1879a366eeb057083856cc812120b
Hashir Mahmood <166294183+Hashir-Mahmood-704@users.noreply.github.com>
2024-09-13 20:44:42 +0500

    did some responsive changes again

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 const RecipeCard = (props) => {
     const {image, id, name, cuisine, servings} = props
     return <Link to={`/details/${id}`} className="border pb-[5px] sm:pb-[15px] shadow-box rounded-[10px] w-[160px] sm:w-[320px]">
-        <img src={image} alt="image" className="w-full h-[200px] object-cover"/>
+        <img src={image} alt="image" className="w-full h-[120px] sm:h-[300px] object-cover"/>
         <div className="mt-[5px] sm:mt-[20px] px-[10px] sm:px-[15px]">
             <p className="font-semibold text-[14px] sm:text-[20px]">{name}</p>
             <div className="mt-[5px] sm:mt-[10px] w-full flex flex-col sm:flex-row justify-between">
